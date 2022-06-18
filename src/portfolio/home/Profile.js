89dced36c1b6866eda13a2b8f798fc36e075ab59
panjiram <id.panjiramadhan@gmail.com>
2022-06-18 20:10:28 +0700

    18juni

---
+++ src/portfolio/home/Profile.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+export default function Profile() {
+  return (
+    <div className='profil-container'>
+        <div className='profil-parent'>
+            <div className='profil-details'>
+                <div className='colz'>
+                    <a href='https://twitter.com/Panjira_'>
+                        <i className='fa fa-twitter-square'></i>
+                    </a>
+                    <a href='https://www.linkedin.com/in/maspanji/'>
+                        <i className='fa fa-linkedin-square'></i>
+                    </a>
+                    <a href='https://www.instagram.com/maspanji_/'>
+                        <i className='fa fa-instagram'></i>
+                    </a>
+                    <a href='https://www.youtube.com/channel/UCAfUOSWnkJNn_h_grX-CQeg'>
+                        <i className='fa fa-youtube-square'></i>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
